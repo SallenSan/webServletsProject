e966ed36a64060c893bfaf401952aac1736d58e8
Lucashtoledo <lucashtoledo97p@gmail.com>
2024-07-15 19:51:49 -0300

    commit2

--- biblioteca/src/main/java/org/academy/model/Livro.java
+++ biblioteca/src/main/java/org/academy/model/Livro.java
@@ -1,4 +1,16 @@
 package org.academy.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+
 public class Livro {
+
 }
